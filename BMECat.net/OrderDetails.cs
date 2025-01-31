8397636b767d8ca9b56bfe70f692afd931c283de
Stephan <stephan.stapel@web.de>
2025-01-31 07:39:45 +0100

    add documentation

--- BMECat.net/OrderDetails.cs
+++ BMECat.net/OrderDetails.cs
@@ -22,19 +22,22 @@ using System.Runtime.InteropServices;
 using System.Text;
 
 namespace BMECat.net
-{       
+{
+    /// <summary>
+    /// Bestelleinheit
+    /// 
+    /// Einheit, in der das Produkte bestellt werden kann; es können nur Vielfache dieser Einheit
+    /// bestellt werden.
+    /// Auf diese Einheit (oder auf Teile oder auf Vielfache davon) bezieht sich stets auch der
+    /// Preis.
+    /// Beispiel: Kiste Mineralwasser mit 6 Flaschen
+    /// Bestelleinheit: "Kiste", Inhaltseinheit/Einheit des Artikels: "Flasche"
+    /// Verpackungsmenge: "6"
+    /// </summary>
     public class OrderDetails
     {
         /// <summary>
-        /// Bestelleinheit
-        /// 
-        /// Einheit, in der das Produkte bestellt werden kann; es können nur Vielfache dieser Einheit
-        /// bestellt werden.
-        /// Auf diese Einheit (oder auf Teile oder auf Vielfache davon) bezieht sich stets auch der
-        /// Preis.
-        /// Beispiel: Kiste Mineralwasser mit 6 Flaschen
-        /// Bestelleinheit: "Kiste", Inhaltseinheit/Einheit des Artikels: "Flasche"
-        /// Verpackungsmenge: "6"
+        /// Die ORDER_UNIT ist die Bestelleinheit und gibt an, in welcher Einheit bzw. Einheiten der Artikel bestellbar ist. Auf diese Einheit bezieht sich stets auch der Preis.
         /// </summary>
         public QuantityCode OrderUnit { get; set; } = new QuantityCode();
 
@@ -44,9 +47,25 @@ namespace BMECat.net
         /// Einheit des Produktes innerhalb einer Bestelleinheit
         /// </summary>
         public QuantityCode ContentUnit { get; set; } = new QuantityCode();
+
+        /// <summary>
+        /// Anzahl der Inhaltseinheiten je Bestelleinheit
+        /// </summary>
         public int? ContentUnitPerOrderUnit { get; set; }
+
+        /// <summary>
+        /// Ganze Zahl! Anzahl der grundlegenden Bestelleinheit. Als Standardwert wird „1“ angenommen, sofern keine andere Preismenge angegeben wird
+        /// </summary>
         public int? PriceQuantity { get; set; }
+
+        /// <summary>
+        /// Mindestbestellmenge bezogen auf die Bestellmaßeinheit, nur ganzzahlige Werte ab 1 sind erlaubt. Als Standardwert wird „1“ angenommen, sofern nicht anders angegeben
+        /// </summary>
         public int? QuantityMin { get; set; }
+
+        /// <summary>
+        /// Intervallmenge (bzw. Mengenstaffel), in der das Produkt bestellt werden kann, ausgehend von der Mindestbestellmenge. Als Standardwert wird „1“ angenommen, sofern nicht anders angegeben
+        /// </summary>
         public int? QuantityInterval { get; set;}
     }
 }
